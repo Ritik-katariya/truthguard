07ede4025ea1b3b805c79b71614e1065394669df
Ritik-katariya <153073623+Ritik-katariya@users.noreply.github.com>
2025-02-01 17:53:33 +0530

    hgfgh

--- backend/routes/checkRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { checkContent } = require('../controllers/checkcontroller');
-
-router.post('/', checkContent);
-
-module.exports = router;
